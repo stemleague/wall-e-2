a9fb1cdb6fd260e7976a4aeb4b013b42259d5784
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-09 16:24:44 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+  # This line declares a variable called weight that stores the value that the user gives when ran
+  weight = 3
+
+  # This code checks if weight is greater than 100lbs.
+  if weight >= 100:
+     # TODO: Print a message to tell the user than their item is too heavy for WALL-E to decompose!
+    print("")
+  
+  # This code is ran only if the weight less than 100 lbs
+  else:
+    # User gets the width of the item
+    width = 10
+    # TODO: create a variable called height that stores a value of the height of the trash
+
+    print("Compacting Trash...")
+    # TODO: Compute the dimensions of the trash item
+
+    # TODO: Compute the weight of the trash after it is decomposed (see formula in slides)
+
+    # TODO: Print a message to the user to let them know that item has been decomposed and what is the final weight of the item after decomposed.
